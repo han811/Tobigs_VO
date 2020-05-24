c7d8663f963eccb645335db4dd1e5f00679d87ca
han811 <kimuw42@gmail.com>
2020-05-24 17:43:05 +0900

    0524

--- DOF6_2.py
+++ DOF6_2.py
@@ -7,61 +7,74 @@ import torch.utils as utils
 import torchvision.datasets as dset
 import torchvision.transforms as transforms
 import matplotlib.pyplot as plt
-from params import par
-from torch.nn.init import kaiming_normal_, orthogonal_
+from torchsummary import summary
 
+def pose_loss(output_1,output_2, target_1, target_2):
 
-
-def pose_loss(output, target):
-
-    temp = output[0]
-    temp_2 = output[1]
-    P = torch.dot(temp, temp_2)
-
-
-    taget_t = target[0]
-    target_t_2 = target[1]
-    P_truth = torch.dot(temp,temp_2)
-
-
+    P = torch.dot(output_1, output_2)
+    P_truth = torch.dot(target_1, target_2)
     loss = (P - P_truth)**2
+    return loss 
+
+def now_loss(output_1,target_1):
+    return torch.mean((output_1 - target_1) ** 2)
+
+def my_loss(out, tar):
+
+    loss = 0
+    loss += pose_loss(out[0],out[1],tar[0],tar[1])
+    loss += pose_loss(out[1],out[2],tar[1],tar[2])
+    loss += pose_loss(out[2],out[3],tar[2],tar[3])
+    loss += pose_loss(out[3],out[4],tar[3],tar[4])
+    loss += pose_loss(out[0],out[2],tar[0],tar[2])
+    loss += pose_loss(out[2],out[4],tar[2],tar[4])
+    loss += pose_loss(out[0],out[4],tar[0],tar[4]) 
+    loss += now_loss(out[0],tar[0])
+    loss += now_loss(out[1],tar[1])
+    loss += now_loss(out[2],tar[2])
+    loss += now_loss(out[3],tar[3])
+    loss += now_loss(out[4],tar[4])
     return loss
-
-
-
 
 class Tobi_model(nn.Module):
     def __init__(self):
         super(Tobi_model, self).__init__()
         self.Res = ResNet50()
         self.Res_5 = res_5()
+        self.Res_5_2 = res_5_2()
         self.ELU = nn.ELU()
-        self.rnn = nn.LSTM(input_size=49152, hidden_size=1000, num_layers=2, batch_first=True)
-        self.rnn_2 = nn.LSTM(input_size=49152, hidden_size=1000, num_layers=1, batch_first=True)
+        self.rnn = nn.LSTM(input_size=98304, hidden_size=1000, num_layers=2, batch_first=True)
+        self.rnn_2 = nn.LSTM(input_size=98304, hidden_size=1000, num_layers=1, batch_first=True)
         self.fc1 = nn.Linear(1000, 1024)
         self.fc2 = nn.Linear(2048, 1024)
         self.fc3 = nn.Linear(1024, 1024)
         self.final_1 = nn.Linear(1024, 3)
         self.final_2 = nn.Linear(1024, 4)
 
-    def sequence_1(self, x):
-        x_1 = x[0]  # t-1  step
-        x_2 = x[1]  # t step
+    def forward(self, x):
+        x_1 = x[0].unsqueeze(0)  # t-1  step
+        x_2 = x[1].unsqueeze(0) # t step
         
         #Residual block pass 
         x_1 = self.Res(x_1) 
         x_2 = self.Res(x_2)
 
         #RCNN1
-        x_3 = torch.cat([x_2,x_2],dim = 0)
+        x_3 = torch.cat([x_1,x_2],dim = 1)
+        x_3 = self.Res_5_2(x_3)
         x_3, h_c = self.rnn(x_3)
         x_3 = self.fc1(x_3)
         #RCNN2
+        x_2 = self.Res_5(x_2)
         x_2, h_c_2 = self.rnn_2(x_2)
         x_2 = self.fc1(x_2)
         x_2 = self.ELU(x_2)
         #FC layer
-        x_3 = torch.cat([x_2,x_3],dim = 0)
+        x_2 = x_2.view(1,1024)
+        x_3 = x_3.view(1,1024)
+        x_3 = torch.cat([x_2,x_3],dim = 1)
+        # print(x_2.size())
+        # print(x_3.size())
         x_3 = self.fc2(x_3)
 
         #Translation
@@ -71,10 +84,10 @@ class Tobi_model(nn.Module):
         #Quanternion
         x_5 = self.fc3(x_3)
         x_5 = self.final_2(x_3)
-        out = torch.cat([x_2,x_3], dim = 0)
+        out = torch.cat([x_4,x_5], dim = 1)
         return out
 
-    def forward(self, x)
+    # def forward(self, x)
 
 
 
@@ -174,12 +187,42 @@ class ResNet_5(nn.Module):
         out = self.layer1_(x)
         out = self.ELU(out)
         out = nn.functional.avg_pool2d(out, 4)
-        out = out.view(out.size(0), -1)
-        out = self.linear(out)
+        out = out.view(1,out.size(0), -1)
+        # out = self.linear(out)
+        return out
+
+class ResNet_5_2(nn.Module):
+    def __init__(self, block):
+        super(ResNet_5_2, self).__init__()
+        self.in_planes = 2048
+        self.conv1 = nn.Conv2d(512, 64, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)        
+        self.layer1_ = self._make_layer(block, 512, 3, stride=2)
+        self.ELU = nn.ELU()
+        self.linear = nn.Linear(2048, 1024)
+        
+    def _make_layer(self, block, planes, num_blocks, stride):
+        strides = [stride] + [1]*(num_blocks-1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.in_planes, planes, stride))
+            self.in_planes = planes * block.expansion
+        return nn.Sequential(*layers)
+
+    # 5번쨰 Resdual BLock후, 펴주자
+    def forward(self, x):
+        out = self.layer1_(x)
+        out = self.ELU(out)
+        out = nn.functional.avg_pool2d(out, 4)
+        out = out.view(1,out.size(0), -1)
+        # out = self.linear(out)
         return out
 
 def res_5():
     return  ResNet_5(Bottleneck)
+
+def res_5_2():
+    return  ResNet_5_2(Bottleneck)
 
 def ResNet50():
     return ResNet(Bottleneck, [3, 4, 6, 3])
@@ -187,47 +230,33 @@ def ResNet50():
 def test():
     net = ResNet18()
     y = net(torch.randn(1, 3, 32, 32))
-    print(y.size())
-
-
+    # print(y.size())
 
 
 class loss_cal(nn.Module):
-    def __init__(self,block):
+    def __init__(self,label):
         super(loss_cal, self).__init__()
         self.out_tensor=torch.ones(5,7)
-        self.num=0
+        self.label = label
+        self.num = 0
 
     def add_loss(self, out_):
         for i in range(5):
             if i!=4:
                 self.out_tensor[i] = self.out_tensor[i+1]
             else:
+                # print(out_.size())
                 self.out_tensor[i] = out_
         self.num+=1
     
-    def calculate_loss(self):
+    def calculate_loss(self, out__):
         if self.num>=5:
-            loss = my_lose(self.out_tensor)
+            self.add_loss(out__)
+            loss = my_loss(self.out_tensor,self.label[self.num:self.num+5,:])
             return loss
         else:
+            self.add_loss(out__)
             pass
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -237,6 +266,7 @@ class loss_cal(nn.Module):
 data_size = 64
 batch_size = 1
 input = torch.rand(data_size,1,3,256,192)
+# input = input.int()
 
 resnet1_4 = ResNet50()
 resnet5 = res_5()
@@ -247,22 +277,52 @@ fin_out_ = 0
 im_data_stack = []
 im_data_stack_2 = []
 
-for i in range(int(data_size/batch_size)):
-    if i==0:
-        im_data_stack.append(input[i])
-        pre_out_ = resnet1_4.forward(im_data_stack[0])
-        pre_out_ = resnet5.forward(pre_out_)
-    elif i==1:
-        im_data_stack.append(input[i])
-        out_ = resnet1_4.forward(im_data_stack[1])
-        out_ = resnet5.forward(out_)
-        fin_out_ = torch.cat([pre_out_,out_],dim=0)
-    else:
-        pre_out_ = out_
-        del im_data_stack[0]
-        im_data_stack.append(input[i])
-        out_ = resnet1_4.forward(im_data_stack[1])
-        out_ = resnet5.forward(out_)
-        fin_out_ = torch.cat([pre_out_,out_],dim=0)
-        print(fin_out_.size()) 
+label = torch.ones(data_size,7)
+
+loss_ = loss_cal(label)
+
+# for i in range(int(data_size/batch_size)):
+#     if i==0:
+#         im_data_stack.append(input[i])
+#         pre_out_ = resnet1_4.forward(im_data_stack[0])
+#         pre_out_ = resnet5.forward(pre_out_)
+#     elif i==1:
+#         im_data_stack.append(input[i])
+#         out_ = resnet1_4.forward(im_data_stack[1])
+#         out_ = resnet5.forward(out_)
+#         fin_out_ = torch.cat([pre_out_,out_],dim=0)
+#     else:
+#         pre_out_ = out_
+#         del im_data_stack[0]
+#         im_data_stack.append(input[i])
+#         out_ = resnet1_4.forward(im_data_stack[1])
+#         out_ = resnet5.forward(out_)
+#         fin_out_ = torch.cat([pre_out_,out_],dim=0)
+#         print(fin_out_.size()) 
+
+tobiVO = Tobi_model().to('cuda')
+# tobiVO = Tobi_model()
+cal_loss = 0
+torch.save(tobiVO,'./modelsize')
+# print(tobiVO.eval())
+summary(tobiVO,(3,256,192))
+with torch.no_grad():
+    for i in range(input.size()[0]):
+        temp_loss = 0
+
+        if i>=1:
+            input_ = torch.cat([input[i-1],input[i]],dim=0)
+            finout = tobiVO.forward(input_)
+            # print(loss_.num)
+
+            if(loss_.num>=5):
+                # print('hi')
+                temp_loss = loss_.calculate_loss(finout)
+                cal_loss += temp_loss
+                print(cal_loss)
+            else:
+                loss_.calculate_loss(finout)
+        
+    
+
 
