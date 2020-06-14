d4736edd6731737c103f7184fbc1dcd0187b2ac8
pjh <pjh_1996@naver.com>
2020-06-14 15:55:56 +0900

    modify scheduler

--- tobi/main.py
+++ tobi/main.py
@@ -19,44 +19,48 @@ def run(model,data,label,batch,optimizer,epoch,scheduler):
     for j in range(epoch):
         for i in range(n):
             if i==(n-1):
-                # input = torch.tensor(data[i*batch:(i+1)*batch+r],requires_grad=True)
-                # input = torch.reshape(input,(6,1,3,224,224))
-                input = torch.tensor(data[i*batch:(i+1)*batch+r],requires_grad=True).cuda()
-                input = torch.reshape(input,(6,1,3,224,224)).cuda()
+                input = torch.tensor(data[i*batch:(i+1)*batch+r],requires_grad=True)
+                input = torch.reshape(input,(6,1,3,224,224))
+                # input = torch.tensor(data[i*batch:(i+1)*batch+r],requires_grad=True).cuda()
+                # input = torch.reshape(input,(6,1,3,224,224)).cuda()
                 label_ = label[i*batch:(i+1)*batch+r][0]
             else:
-                # input = torch.tensor(data[i*batch:(i+1)*batch],requires_grad=True)
-                # input = torch.reshape(input,(6,1,3,224,224))
-                input = torch.tensor(data[i*batch:(i+1)*batch],requires_grad=True).cuda()
-                input = torch.reshape(input,(6,1,3,224,224)).cuda()
+                input = torch.tensor(data[i*batch:(i+1)*batch],requires_grad=True)
+                input = torch.reshape(input,(6,1,3,224,224))
+                # input = torch.tensor(data[i*batch:(i+1)*batch],requires_grad=True).cuda()
+                # input = torch.reshape(input,(6,1,3,224,224)).cuda()
                 label_ = label[i*batch:(i+1)*batch][0]
-            #scheduler.zero_grad()
             optimizer.zero_grad()
-            # loss = model.get_loss(input,torch.from_numpy(label_[1:6]))
-            loss = model.get_loss(input,torch.from_numpy(label_[1:6]).cuda())
+            loss = model.get_loss(input,torch.from_numpy(label_[1:6]))
+            # loss = model.get_loss(input,torch.from_numpy(label_[1:6])).cuda())
             loss.backward(retain_graph = True)
-            #scheduler.step()
             optimizer.step()
             print(loss)
+        scheduler.step()
         torch.save(tobiVO.state_dict(),'./modelsize'+str(j)+'.pth')
 
-data = np.load('/home/kth/Desktop/deepVO/Tobigs_VO/tobi/data/data/x0.npy')
-data_label = np.load('/home/kth/Desktop/deepVO/Tobigs_VO/tobi/data/label/x_label0.npy')
+data = np.load('/home/pjh/다운로드/data/data/x0.npy')
+data_label = np.load('/home/pjh/다운로드/data/label/x_label0.npy')
+
+# data_ = np.random.rand(1002,3,224,224)
+# data_label_ = np.random.rand(1002,7)
+
 data = np.reshape(data,(data.shape[0]//6,6,3,224,224))
 data_label = np.reshape(data_label,(data_label.shape[0]//6,6,7))
 
+print(data.dtype)
 # tobiVO = Tobi_model()
-tobiVO = Tobi_model().cuda()
-tobiVO.load_state_dict(torch.load('/home/kth/Desktop/deepVO/Tobigs_VO/tobi/modelsize4.pth'))
-tobiVO.eval()
+tobiVO = Tobi_model()#.cuda()
+#tobiVO.load_state_dict(torch.load('/home/pjh/Tobigs_VO/tobi/modelsize4.pth'))
+#tobiVO.eval()
+input = torch.tensor(data[100:101],requires_grad=True)#.cuda()
+input = torch.reshape(input,(6,1,3,224,224))#.cuda()
+label = torch.tensor(data_label[100:101][0][1])#.cuda()
 
-input = torch.tensor(data[100:101],requires_grad=True).cuda()
-input = torch.reshape(input,(6,1,3,224,224)).cuda()
-label = torch.tensor(data_label[100:101][0][1]).cuda()
+
 answer = tobiVO.forward(torch.cat([input[0],input[1]],dim = 0))
-print((answer-label))
-# optimizer = optim.Adam(tobiVO.parameters(), lr=0.0001)
-# scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=400, gamma=0.5)
+optimizer = optim.Adam(tobiVO.parameters(), lr=0.00001)
+scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=400, gamma=0.5)
 
-# if __name__ == "__main__":
-#     run(model=tobiVO,data=data,label=data_label,batch=1,optimizer=optimizer,epoch=20,scheduler=scheduler)
+if __name__ == "__main__":
+    run(model=tobiVO,data=data,label=data_label,batch=1,optimizer=optimizer,epoch=20,scheduler=scheduler)
