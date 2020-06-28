4582e76f290c75dc07311b51af17d3ea312ce49f
cryingjin <41279475+cryingjin@users.noreply.github.com>
2020-06-28 15:40:30 +0900

    upload colab files

--- visualize.py
+++ visualize.py
@@ -1,70 +1,73 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import time
-import os
-from data_helper import get_data_info_tobi, ImageSequenceDataset, ImageSequenceDataset_visual
 from params import par
 
-########################
-### variable setting ###
-########################
-
 pose_GT_dir = par.pose_dir  #'KITTI/pose_GT/'
-predicted_result_dir = os.getcwd()+'/image/'
-predict_path = os.getcwd()+'/result/'
+predicted_result_dir = './result/'
+gradient_color = True
 
-train_df = get_data_info_tobi(folder_list=par.train_video, overlap=False)	
-train_dataset = ImageSequenceDataset(train_df, par.resize_mode, (par.img_w, par.img_h), par.img_means, par.img_stds, par.minus_point_5)
-train_dataset_gt = ImageSequenceDataset_visual(train_df, par.resize_mode, (par.img_w, par.img_h), par.img_means, par.img_stds, par.minus_point_5)
-	
-x_idx = 2
-y_idx = 0
-
-############################
-### variable setting end ###
-############################
-
-def plot_route(gt, c_gt='g', c_out='r'):
+def plot_route(gt, out, c_gt='g', c_out='r'):
+	x_idx = 3
+	y_idx = 5
 	x = [v for v in gt[:, x_idx]]
 	y = [v for v in gt[:, y_idx]]
-	print(x[0:20])
-	print(y[0:20])
-
 	plt.plot(x, y, color=c_gt, label='Ground Truth')
-	plt.savefig(predicted_result_dir+str(video))
-	# plt.show()
+	#plt.scatter(x, y, color='b')
 
-def plot_route_predict(out, c_gt='g', c_out='r'):
-	x_temp=0
-	y_temp=0
-	x=[]
-	y=[]
-	print(out.shape)
-	for i in range(out.shape[0]):
-		x_temp+=out[i][x_idx] #/100
-		y_temp+=out[i][y_idx] #/100
-		x+=[x_temp]
-		y+=[y_temp]
+	x = [v for v in out[:, x_idx]]
+	y = [v for v in out[:, y_idx]]
+	plt.plot(x, y, color=c_out, label='DeepVO')
+	#plt.scatter(x, y, color='b')
+	plt.gca().set_aspect('equal', adjustable='datalim')
 
-	# x = [v+x_temp for v in out[:, x_idx]]
-	# y = [v+y_temp for v in out[:, y_idx]]
-	print(sum(x[0:20]))
-	print(sum(y[0:20]))
-	print(y[0:20])
-	# x /= 100
-	# y /= 100
 
-	plt.plot(x, y, color=c_out, label='Predict')
-	plt.savefig(predicted_result_dir+'predict')
+# Load in GT and predicted pose
+# video list 04,10 test data 사용
+# video_list = ['00', '02', '08', '09']
+# video_list += ['01', '04', '05', '06', '07', '10']
+video_list = ['04', '10']
 
-if __name__=='__main__':
-	# Load in GT and predicted pose
-	video_list = ['00']
-	for video in video_list:
-		predict = np.load(predict_path+'x_predict.npy')
-		GT_pose_path = '{}{}.npy'.format(pose_GT_dir, video)
-		gt = np.load(GT_pose_path)
-		plot_route(gt, c_gt='g', c_out='r')
-		plt.close()
-		plot_route_predict(predict, c_gt='g', c_out='r')
-		plt.close()
+for video in video_list:
+	print('='*50)
+	print('Video {}'.format(video))
+
+	GT_pose_path = '{}{}.npy'.format(pose_GT_dir, video)
+	gt = np.load(GT_pose_path)
+	pose_result_path = '{}out_{}.txt'.format(predicted_result_dir, video)
+	with open(pose_result_path) as f_out:
+		out = [l.split('\n')[0] for l in f_out.readlines()]
+		for i, line in enumerate(out):
+			out[i] = [float(v) for v in line.split(',')]
+		out = np.array(out)
+		mse_rotate = 100 * np.mean((out[:, :3] - gt[:, :3])**2)
+		mse_translate = np.mean((out[:, 3:] - gt[:, 3:6])**2)
+		print('mse_rotate: ', mse_rotate)
+		print('mse_translate: ', mse_translate)
+
+	
+	if gradient_color:
+		# plot gradient color
+		step = 200
+		plt.clf()
+		plt.scatter([gt[0][3]], [gt[0][5]], label='sequence start', marker='s', color='k')
+		for st in range(0, len(out), step):
+			end = st + step
+			g = max(0.2, st/len(out))
+			c_gt = (0, g, 0)
+			c_out = (1, g, 0)
+			plot_route(gt[st:end], out[st:end], c_gt, c_out)
+			if st == 0:
+				plt.legend()
+			plt.title('Video {}'.format(video))
+			save_name = '{}route_{}_gradient.png'.format(predicted_result_dir, video)
+		plt.savefig(save_name)
+	else:
+		# plot one color
+		plt.clf()
+		plt.scatter([gt[0][3]], [gt[0][5]], label='sequence start', marker='s', color='k')
+		plot_route(gt, out, 'r', 'b')
+		plt.legend()
+		plt.title('Video {}'.format(video))
+		save_name = '{}route_{}.png'.format(predicted_result_dir, video)
+		plt.savefig(save_name)
